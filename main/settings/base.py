7e8eb9965961e71982b1a589f3842ca2eff36d80
MELTEM <meltemfsanaldi@gmail.com>
2023-01-13 00:46:33 +0300

    profil

--- main/settings/base.py
+++ main/settings/base.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,18 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    #thirdparty apps
-    'rest_framework', #! buraya ekle
+    #my app
+    'rest_framework',
     'drf_yasg',
     'rest_framework.authtoken',
     'dj_rest_auth',
-    # 'debug_toolbar',
     'users',
+
 ]
 
 MIDDLEWARE = [
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,57 +131,72 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# INTERNAL_IPS = [ 
-#     "127.0.0.1", 
+# INTERNAL_IPS = [
+#     "127.0.0.1",
 # ]
+# DEBUG_TOOLBAR_CONFIG = {
+#     'DISABLE_PANELS': [
+#         'debug_toolbar.panels.redirects.RedirectsPanel',
+#     ],
+#     'SHOW_TEMPLATE_CONTEXT': True,
+# }
 
-LOGGING = { 
-    "version": 1, 
-    # is set to True then all loggers from the default configuration will be disabled. 
-    "disable_existing_loggers": True, 
-    # Formatters describe the exact format of that text of a log record.  
-    "formatters": { 
-        "standard": { 
-            "format": "[%(levelname)s] %(asctime)s %(name)s: %(message)s" 
-        }, 
-        'verbose': { 
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}', 
-            'style': '{', 
-        }, 
-        'simple': { 
-            'format': '{levelname} {message}', 
-            'style': '{', 
-        }, 
-    }, 
-    # The handler is the engine that determines what happens to each message in a logger. 
-    # It describes a particular logging behavior, such as writing a message to the screen,  
-    # to a file, or to a network socket. 
-    "handlers": { 
-        "console": { 
-            "class": "logging.StreamHandler", 
-            "formatter": "standard", 
-            "level": "INFO", 
-            "stream": "ext://sys.stdout", 
-            }, 
-        'file': { 
-            'class': 'logging.FileHandler', 
-            "formatter": "verbose", 
-            'filename': './debug.log', 
-            'level': 'INFO', 
-        }, 
-    }, 
-    # A logger is the entry point into the logging system. 
-    "loggers": { 
-        "django": { 
+# import mimetypes
+# mimetypes.add_type("application/javascript", ".js", True)
+# django-debug-toolbar
+# import mimetypes
+
+# mimetypes.add_type("application/javascript", ".js", True)
+LOGGING = {
+    "version": 1, # is set to True then all loggers from the default configuration will be disabled.
+    "disable_existing_loggers": True,
+    # Formatters describe the exact format of that text of a log record. 
+    "formatters": {
+        "standard": {
+            "format": "[%(levelname)s] %(asctime)s %(name)s: %(message)s"
+        },
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    # The handler is the engine that determines what happens to each message in a logger.
+    # It describes a particular logging behavior, such as writing a message to the screen, 
+    # to a file, or to a network socket.
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler","formatter": "standard",
+            "level": "INFO",
+            "stream": "ext://sys.stdout",
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            "formatter": "verbose",
+            'filename': './debug.log',
+            'level': 'INFO',
+        },
+ },
+ # A logger is the entry point into the logging system.
+    "loggers": {
+        "django": {
             "handlers": ["console", 'file'], 
-            # log level describes the severity of the messages that the logger will handle.  
-            "level": config("DJANGO_LOG_LEVEL"), 
-            'propagate': True, 
-            # If False, this means that log messages written to django.request  
-            # will not be handled by the django logger. 
-        }, 
-    }, 
+            # log level describes the severity of the messages that the logger will handle. 
+            "level": config("DJANGO_LOG_LEVEL", "INFO"),
+            'propagate': True,
+
+            # istersem 
+            # DJANGO_LOG_LEVEL=ERROR şeklinde .envde ekleyebilirim
+            
+            # If False, this means that log messages written to django.request 
+            #  will not be handled by the django logger.
+        },
+    },
 }
+# handlers iki farklı handler işlemi yapan loji kuran 
 
 
 REST_FRAMEWORK = {
@@ -193,4 +207,11 @@ REST_FRAMEWORK = {
 
 REST_AUTH_SERIALIZERS = {
     'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
+    
 }
+
+# AUTH_USER_MODEL = 'users.MyUser'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / STATIC_URL
+MEDIA_ROOT = BASE_DIR / "pictures"
